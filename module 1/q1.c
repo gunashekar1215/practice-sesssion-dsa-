d806cd4687b67511a672fe9354d85fb279064bf6
gunashekar1215 <gunashekarcsk@gmail.com>
2025-11-05 23:58:27 +0530

    Implement polynomial representation using linked list

---
+++ module 1/q1.c
@@ -0,0 +1,42 @@
+#Represent a polynomial using linked list
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int coeff, exp;
+    struct Node *next;
+};
+
+struct Node* insert(struct Node *head, int c, int e) {
+    struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->coeff = c;
+    temp->exp = e;
+    temp->next = NULL;
+
+    if (head == NULL) return temp;
+
+    struct Node *ptr = head;
+    while (ptr->next != NULL) ptr = ptr->next;
+    ptr->next = temp;
+    return head;
+}
+
+void display(struct Node *head) {
+    while (head != NULL) {
+        printf("%dx^%d", head->coeff, head->exp);
+        head = head->next;
+        if (head != NULL) printf(" + ");
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node *poly = NULL;
+    poly = insert(poly, 3, 2);
+    poly = insert(poly, 5, 1);
+    poly = insert(poly, 6, 0);
+
+    printf("Polynomial: ");
+    display(poly);
+    return 0;
+}
